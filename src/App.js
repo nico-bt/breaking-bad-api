7c5ebbe002fad73b29a3250daad5593c37956014
nico-bt <nico-battaglia@hotmail.com>
2022-04-02 05:22:34 +0000

    Add all characters grid, fetching to the breakingBadAPI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, {useState, useEffect} from "react"
 import './App.css';
-import Header from "./components/ui/Header.js"
+import Header from "./components/ui/Header"
+import CharacterGrid from "./components/characters/CharacterGrid"
 
 function App() {
+	const [items, setItems] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+	
+	useEffect(()=>{
+		const fetchItems = async ()=>{
+			try{
+				const data = await fetch("https://www.breakingbadapi.com/api/characters")
+				const dataArray = await data.json()
+				console.log(dataArray)
+				setItems(dataArray)
+				setIsLoading(false)
+			} catch(err){
+				console.log(err)
+			}
+		}
+		fetchItems()
+	}, [])
+	
   return (
     <div className="container">
 		  <Header />
+		  <CharacterGrid items={items} isLoading={isLoading} />
+		
     </div>
   );
 }
