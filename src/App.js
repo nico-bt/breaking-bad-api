5b77daec8f026d0c501c76d3ca02401fbd971f54
nico-bt <nico-battaglia@hotmail.com>
2022-04-02 03:23:17 +0000

    Add header with logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./components/ui/Header.js"
 
 function App() {
   return (
     <div className="container">
-		  Starting point
+		  <Header />
     </div>
   );
 }
