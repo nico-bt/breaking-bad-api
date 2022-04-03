e2128ecc6661b0afcd09680f26544151dec23a66
nico-bt <nico-battaglia@hotmail.com>
2022-04-03 01:41:54 +0000

    Add error handling in case fetching fails

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ function App() {
 	const [items, setItems] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
 	const [query, setQuery] = useState("")
+	const [errorMsg, setErrorMsg] = useState("")
 	
 	useEffect(()=>{
 		const fetchItems = async ()=>{
 			try{
 				const data = await fetch(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 				const dataArray = await data.json()
-				console.log(dataArray)
 				setItems(dataArray)
 				setIsLoading(false)
+				setErrorMsg("")
 			} catch(err){
 				console.log(err)
+				setErrorMsg("Something went wrong. Please, try again")
+				setIsLoading(false)
 			}
 		}
 		fetchItems()
@@ -28,7 +31,7 @@ function App() {
     <div className="container">
 		  <Header />
 		  <SearchBar getQuery={(formInput)=>setQuery(formInput)}/>
-		  <CharacterGrid items={items} isLoading={isLoading} />
+		  <CharacterGrid items={items} isLoading={isLoading} errorMsg={errorMsg}/>
 		
     </div>
   );
--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,15 +1,17 @@
 import React from "react"
 import CharacterItem from "./CharacterItem"
 
-const CharacterGrid = ({items, isLoading})=> {
-	return isLoading? 
-		(<h1 className="center">Loading...</h1>) 
-		: 
-		(<section className="cards">
-			{items.map(item=>(
-				 <CharacterItem key={item.char_id} item={item} />
-			 ))}
-		 </section>)
+const CharacterGrid = ({items, isLoading, errorMsg})=> {
+	
+	return isLoading? (<h1 className="center">Loading...</h1>) : (
+		
+		errorMsg? (<h1 className="center">{errorMsg}</h1>) : 
+			(<section className="cards">
+				{items.map(item=>(
+					 <CharacterItem key={item.char_id} item={item} />
+				 ))}
+			</section>)
+	)
 }
 
 export default CharacterGrid
